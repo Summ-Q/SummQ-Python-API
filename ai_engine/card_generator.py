3324e51d644bc806c74c0bc1017d80358f591668
Mohamed Nour <mnour70789@gmail.com>
2026-08-19 10:19:09 +0300

    chore: clean up root directory, secure api keys, and update requirements

--- ai_engine/card_generator.py
+++ ai_engine/card_generator.py
@@ -3,6 +3,9 @@ from google import genai
 from google.genai import types
 import json
 import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
